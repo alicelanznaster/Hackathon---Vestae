152342f01a990c76d757cec5f5e7151f84720195
alicelanznaster <alicelanznaster@gmail.com>
2026-06-30 16:50:37 -0300

    FEAT: início das rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CartPanel from '@/components/cart/CartPanel.vue'
+import FemininoView from '@/components/layout/FemininoView.vue'
+import MasculinoView from '@/components/layout/MasculinoView.vue'
+import AcessoriosView from '@/components/layout/AcessoriosView.vue'
+import CalcadosView from '@/components/layout/CalcadosView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +15,29 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/sacola',
+      name: 'sacola',
+      component: CartPanel,
+    }, 
+    {
+      path: '/feminino',
+      name: 'feminino',
+      component: FemininoView,
+    },
+    {
+      path: '/masculino',
+      name: 'masculino',
+      component: MasculinoView,
+    },
+    {
+      path: '/acessorios',
+      name: 'acessorios',
+      component: AcessoriosView,
+    },
+    {
+      path: '/calcados',
+      name: 'calcados',
+      component: CalcadosView,
     },
   ],
 })

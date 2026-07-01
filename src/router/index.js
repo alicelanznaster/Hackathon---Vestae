bb39d0eeff0f19b006159577add7785324e23f15
alicelanznaster <alicelanznaster@gmail.com>
2026-07-01 16:01:48 -0300

    FEAT: término das rotas

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,12 @@ import FemininoView from '@/components/layout/FemininoView.vue'
 import MasculinoView from '@/components/layout/MasculinoView.vue'
 import AcessoriosView from '@/components/layout/AcessoriosView.vue'
 import CalcadosView from '@/components/layout/CalcadosView.vue'
+import SobreView from '@/components/layout/SobreView.vue'
+import ProductList from '@/components/products/ProductList.vue'
+import FavoritosView from '@/components/layout/FavoritosView.vue'
+import ProdutoDetail from '@/components/layout/ProdutoDetail.vue'
+import AnunciarView from '@/components/layout/AnunciarView.vue'
+import PerfilView from '@/components/layout/PerfilView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,6 +44,36 @@ const router = createRouter({
       path: '/calcados',
       name: 'calcados',
       component: CalcadosView,
+    },
+    {
+      path:'/sobre',
+      name:'sobre',
+      component: SobreView,
+    },
+    {
+      path: '/produtos',
+      name: 'products',
+      component: ProductList,
+    },
+    {
+      path: '/favoritos',
+      name: 'favoritos',
+      component: FavoritosView,
+    },
+    {
+      path: '/produto/:id',
+      name: 'produto',
+      component: ProdutoDetail,
+    },
+    {
+      path: '/anunciar',
+      name: 'anunciar',
+      component: AnunciarView,
+    },
+    {
+      path: '/perfil',
+      name: 'perfil',
+      component: PerfilView,
     },
   ],
 })

2d3feb6a27c2bf2f954d5992864a1853affa3d14
flaviabusa <flaviabusacorrea@gmail.com>
2026-07-03 11:49:50 -0300

    FEAT: produto card e produto list finalizados

---
+++ src/components/products/ProductList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import produtos from '@/data/product'
+import ProductCard from './ProductCard.vue'
+</script>
+
+<template>
+    <div class="product-list">
+        <ProductCard v-for="produto in produtos" :key="produto.id" :produto="produto" />
+    </div>
+</template>
+
+<style scoped>
+.product-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 30px;
+    padding: 30px;
+}
+</style>

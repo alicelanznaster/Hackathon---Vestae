811b6a677c5ae824bb7afd45f0a53d4709c57ceb
flaviabusa <flaviabusacorrea@gmail.com>
2026-07-07 16:52:20 -0300

    FEAT: continuação do header

--- src/components/layout/header/HeaderNavbar.vue
+++ src/components/layout/header/HeaderNavbar.vue
@@ -1,4 +1,13 @@
 <script setup>
-
+import { RouterLink } from 'vue-router';
 </script>
 
+<template>
+    <div class="categorias">
+        <RouterLink>
+            
+        </RouterLink>
+    </div>
+</template>
+
+<style scoped></style>

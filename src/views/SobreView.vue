f337547fb1580d7ef133ddd2a9f179ff18ae3acb
flaviabusa <flaviabusacorrea@gmail.com>
2026-08-14 11:25:30 -0300

    STYLE: resoponsividade da página 'sobre'

--- src/views/SobreView.vue
+++ src/views/SobreView.vue
@@ -58,73 +58,144 @@
 </template>
 
 <style scoped>
-.sobre{
-    max-width: 900px;
-    margin: 50px auto;
-    padding: 20px;
-    cursor: default;
+.sobre {
+  max-width: 900px;
+  margin: 50px auto;
+  padding: 20px;
+  cursor: default;
 }
 
 .projeto,
-.equipe{
-    text-align: center;
-    cursor: default;
+.equipe {
+  text-align: center;
+  cursor: default;
 }
 
-h2{
-    margin-bottom: 30px;
-    font-size: 3rem;
-    color: black;
-    cursor: default;
+h2 {
+  margin-bottom: 30px;
+  font-size: 3rem;
+  color: black;
+  cursor: default;
 }
 
-p{
-    max-width: 700px;
-    margin: 0 auto 25px;
-    line-height: 1.7;
-    font-size: 22px;
-    color: black;
-    cursor: default;
+p {
+  max-width: 700px;
+  margin: 0 auto 25px;
+  line-height: 1.7;
+  font-size: 22px;
+  color: black;
+  cursor: default;
 }
 
-hr{
-    margin: 50px 0;
-    border: none;
-    border-top: 2px solid #a19a9a;
-    cursor: default;
+hr {
+  margin: 50px 0;
+  border: none;
+  border-top: 2px solid #a19a9a;
+  cursor: default;
 }
 
-.membros{
-    display: flex;
+.membros {
+  gap: 35px;
+  margin-top: 35px;
+  cursor: default;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+}
+
+.membro {
+  text-align: center;
+  cursor: default;
+
+}
+
+.membro img {
+  margin-top: 20px;
+  width: 180px;
+  height: 180px;
+  border-radius: 50%;
+  object-fit: cover;
+  transition: .3s;
+  cursor: default;
+}
+
+.membro img:hover {
+  transform: scale(1.08);
+}
+
+.membro p {
+  margin: 0;
+  font-size: 25px;
+  font-family: "Marcellus", serif;
+  color: black;
+  cursor: default;
+}
+
+@media (max-width:1024px) {
+  h2 {
+    font-size: 2.5rem;
+  }
+
+  .membros {
     justify-content: center;
-    gap: 35px;
-    margin-top: 35px;
-    cursor: default;
+    column-gap: 0;
+  }
+
+  .membro img {
+    width: 140px;
+    height: 140px;
+    margin-top: 18px;
+  }
+
+  .membro p {
+    font-size: 23px;
+  }
 }
 
-.membro{
-    text-align: center;
-    cursor: default;
+@media (max-width:768px) {
+  h2 {
+    margin-bottom: 20px;
+    font-size: 2rem;
+  }
+
+  p {
+    max-width: 600px;
+    line-height: 1.5;
+    font-size: 18px;
+  }
+
+  .membros {
+    grid-template-columns: repeat(2, 140px);
+    justify-content: center;
+    column-gap: 40px;
+    row-gap: 25px;
+  }
+
+  .membro img {
+    width: 140px;
+    height: 140px;
+
+  }
+
+  .membro p {
+    font-size: 23px;
+  }
+
 }
 
-.membro img{
-    width: 180px;
-    height: 180px;
-    border-radius: 50%;
-    object-fit: cover;
-    transition: .3s;
-    cursor: default;
-}
+@media (max-width:490px){
+  .membro img {
+    width: 120px;
+    height: 120px;
 
-.membro img:hover{
-    transform: scale(1.08);
-}
+  }
 
-.membro p{
-    margin: 0;
-    font-size: 25px;
-    font-family: "Marcellus", serif;
-    color: black;
-    cursor: default;
+  .membro p{
+    font-size: 20px;
+  }
+
+  .membros {
+    column-gap: 20px;
+  }
+
 }
 </style>

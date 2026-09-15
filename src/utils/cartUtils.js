fd047ac3e1913cc45f2d50478ba9f8dc5639d181
alicelanznaster <alicelanznaster@gmail.com>
2026-09-15 19:17:45 -0300

    FEAT: carrinho no localStorage

--- src/utils/cartUtils.js
+++ src/utils/cartUtils.js
@@ -3,45 +3,13 @@ import { ref } from 'vue'
 
 const ultimoProdAdd = ref('')
 const mostrarAviso = ref(false)
-const carrinho = ref([
-    {
-        id: 1,
-        titulo: "Blusa Preta Básica",
-        imagem: "/produtos/blusapretafem.png",
-        descricao: "Foi uma peça que usei bastante, mas sempre cuidei bem. Continua em ótimo estado.",
-        categoria: "Feminino",
-        tamanho: "M",
-        condicao: "Usado",
-        marca: "",
-        preco: 39.90
-    },
-    {
-      id: 2,
-      titulo: "Calça Jeans",
-      imagem: "/produtos/calcajeansfem.png",
-      descricao: "Calça confortável e bem conservada. Estou desapegando porque já não faz mais parte do meu estilo.",
-      categoria: "Feminino",
-      tamanho: "40",
-      condicao: "Usado",
-      marca: "",
-      preco: 49.90,
-      favorito: false,
-      status: "Disponível",
-    },
-    {
-      id: 3,
-      titulo: "Calça Rosa",
-      imagem: "/produtos/calcarosafem.png",
-      descricao: "Comprei pela internet, mas acabou não servindo. Nunca foi usada.",
-      categoria: "Feminino",
-      tamanho: "38",
-      condicao: "Novo",
-      marca: "",
-      preco: 39.90,
-      favorito: false,
-      status: "Disponível",
-  },
-])
+const carrinho = ref([])
+
+const carrinhoSalvo = JSON.parse(
+  localStorage.getItem('carrinho') ?? '[]'
+)
+
+carrinho.value = carrinhoSalvo
 
 function addCarrinho(idItem) {
   const item = produtos.value.find((p) => p.id === idItem)
@@ -54,6 +22,11 @@ function addCarrinho(idItem) {
     if (!itemExistente) {
       carrinho.value.push(item)
     }
+
+    localStorage.setItem(
+      'carrinho',
+      JSON.stringify(carrinho.value)
+    )
 
     mostrarAviso.value = true
 
@@ -68,6 +41,11 @@ function removerCarrinho(idItem) {
 
   if (posicao !== -1) {
     carrinho.value.splice(posicao, 1)
+
+    localStorage.setItem(
+      'carrinho',
+      JSON.stringify(carrinho.value)
+    )
   }
 }
 
@@ -84,5 +62,9 @@ function totalCarrinho() {
 
 function limparCarrinho() {
   carrinho.value = []
+  localStorage.setItem( 
+    'carrinho', 
+    JSON.stringify(carrinho.value) 
+  )
 }
 export { ultimoProdAdd, mostrarAviso, carrinho, addCarrinho, removerCarrinho, totalCarrinho, limparCarrinho}
